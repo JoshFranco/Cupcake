ad13a26f49c75d360ac69a09f2494ab51a756f66
Josh Franco <jsfranco127@gmail.com>
2020-06-01 12:13:55 -0700

    Added Xcode proj to git

---
+++ CupcakeCorner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by Josh Franco on 6/1/20.
+//  Copyright © 2020 Josh Franco. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
